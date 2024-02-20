0557764ec9a9f0e73e3ba87b1103273f3dd6ba24
hrai_20 <hrai20051@gmail.com>
2024-02-20 11:49:24 +0530

    second

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const puppeteer = require("puppeteer");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.use(cors());
 
 async function scrapeAmazon(keyword) {
   const browser = await puppeteer.launch({
@@ -11,7 +13,9 @@ async function scrapeAmazon(keyword) {
   await page.goto(`https://www.amazon.in/s?k=${keyword}`);
   const products = await page.evaluate(() => {
     const productList = [];
-    const items = document.querySelectorAll('div[data-component-type="s-search-result"]');
+    const items = document.querySelectorAll(
+      'div[data-component-type="s-search-result"]'
+    );
     const itemsArray = Array.from(items);
     const firstFourItems = itemsArray.slice(0, 4);
 
@@ -24,18 +28,33 @@ async function scrapeAmazon(keyword) {
       }
       const innerUrl = `https://www.amazon.in/product-reviews/${attributes["data-asin"]}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews`;
       const nameElement = item.querySelector("h2");
-      const descriptionElement = item.querySelector("h2.a-size-mini.a-color-base");
+      const descriptionElement = item.querySelector(
+        "h2.a-size-mini.a-color-base"
+      );
       const ratingElement = item.querySelector(".a-icon-star-small");
-      const reviewsElement = item.querySelector(".a-size-base.a-color-secondary");
+      const reviewsElement = item.querySelector(
+        ".a-size-base.a-color-secondary"
+      );
       const priceElement = item.querySelector(".a-price span");
 
       const productName = nameElement ? nameElement.innerText.trim() : "N/A";
-      const productDescription = descriptionElement ? descriptionElement.innerText.trim() : "N/A";
-      const productRating = ratingElement ? ratingElement.innerText.split(" ")[0] : "N/A";
+      const productDescription = descriptionElement
+        ? descriptionElement.innerText.trim()
+        : "N/A";
+      const productRating = ratingElement
+        ? ratingElement.innerText.split(" ")[0]
+        : "N/A";
       const productReviews = reviewsElement ? reviewsElement.innerText : "N/A";
       const productPrice = priceElement ? priceElement.innerText : "N/A";
 
-      productList.push({ productName, productDescription, productRating, productReviews, productPrice, innerUrl });
+      productList.push({
+        productName,
+        productDescription,
+        productRating,
+        productReviews,
+        productPrice,
+        innerUrl,
+      });
     });
     return productList;
   });
@@ -45,13 +64,21 @@ async function scrapeAmazon(keyword) {
       const page = await browser.newPage();
       await page.goto(product.innerUrl);
       const { topReviews, reviewCount } = await page.evaluate(() => {
-        const reviewItems = document.querySelectorAll('div[data-hook="review"]');
+        const reviewItems = document.querySelectorAll(
+          'div[data-hook="review"]'
+        );
         const reviewItemsArray = Array.from(reviewItems);
         const topReviews = reviewItemsArray.map((item) => {
-          return item.querySelector(".a-size-base.review-text.review-text-content").innerText;
+          return item.querySelector(
+            ".a-size-base.review-text.review-text-content"
+          ).innerText;
         });
-        const reviewElement = document.querySelector(".a-row.a-spacing-base.a-size-base");
-        const reviews = reviewElement ? reviewElement.innerHTML.split(", ")[1].split(" ")[0] : "N/A";
+        const reviewElement = document.querySelector(
+          ".a-row.a-spacing-base.a-size-base"
+        );
+        const reviews = reviewElement
+          ? reviewElement.innerHTML.split(", ")[1].split(" ")[0]
+          : "N/A";
         return { topReviews, reviewCount: reviews };
       });
       product.productReviews = reviewCount;
